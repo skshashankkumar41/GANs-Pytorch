59c4d9a3df7345d70589468031bd85189c63e19d
Shashank Prasad <skshashankkumar41@gmail.com>
2020-12-09 01:44:38 +0530

    added discriminator

--- Basic-GAN/discriminator.py
+++ Basic-GAN/discriminator.py
@@ -6,8 +6,17 @@ class Discriminator(nn.Module):
     def __init__(self,im_dim,hidden_dim):
         super(Discriminator,self).__init__()
         self.disc = nn.Sequential(
-
+            self.discriminator_block(im_dim,hidden_dim*4),
+            self.discriminator_block(hidden_dim*4,hidden_dim*2),
+            self.discriminator_block(hidden_dim*2,hidden_dim),
+            nn.Linear(hidden_dim,1)
+        )
+    # discriminator block 
+    def discriminator_block(self,input_dim,output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim,output_dim),
+            nn.LeakyReLU(0.2)
         )
 
-    def discriminator_block(self,input_dim,output_dim):
-        
+    def forward(self,image):
+        return self.disc(image)
