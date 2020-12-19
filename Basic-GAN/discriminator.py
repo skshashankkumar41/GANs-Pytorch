43dac859ec92f2f4b877fcf801a4228e72896126
Shashank Prasad <skshashankkumar41@gmail.com>
2020-12-20 04:49:05 +0530

    minor fix

--- Basic-GAN/discriminator.py
+++ Basic-GAN/discriminator.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 
 # discriminator class to take noise vector and return the image
 class Discriminator(nn.Module):
-    def __init__(self,im_dim,hidden_dim):
+    def __init__(self,img_dim,hidden_dim):
         super(Discriminator,self).__init__()
         self.disc = nn.Sequential(
-            self.discriminator_block(im_dim,hidden_dim*4),
+            self.discriminator_block(img_dim,hidden_dim*4),
             self.discriminator_block(hidden_dim*4,hidden_dim*2),
             self.discriminator_block(hidden_dim*2,hidden_dim),
             nn.Linear(hidden_dim,1)
